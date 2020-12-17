92625326057244be331a61e3c679fbf8db948222
Daniel Alves <danielalves.ccomp@gmail.com>
2020-12-17 19:57:39 -0300

    Refactoring callbacks to promises

--- 2-node/index.js
+++ 2-node/index.js
@@ -2,67 +2,83 @@
  0 - Obter um usuario
  1 - Obter o número de telefone de um usuario a partir de seu id
  2 - Obter o endereco do usuario pelo id
-
 */
+const util = require('util')
+const getEndAsync = util.promisify(getEnd)
 
-function getUser(callback) {
-  setTimeout(function() {
-    return callback(null, {
-      id: 1,
-      name: 'Aladin',
-      birth: new Date()
-    })
-  }, 1000)
+function getUser() {
+
+  return new Promise(function resolvePromise(resolve, reject) {
+    setTimeout(function() {
+      return resolve({
+        id: 1,
+        name: 'Nik',
+        birth: new Date()
+      })
+    }, 1000)
+
+  })
 }
 
-function getTel(idUser, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      tel: '31569456',
-      ddd: '21'
-    })
-  }, 2000)
+function getTel(idUser) {
+  
+  return new Promise(function resolvePromise(resolve, reject) {
+    
+    setTimeout(() => {
+      return resolve({
+        tel: '31569456',
+        ddd: '21'
+      })
+    }, 1000)
+
+  })
+
 }
 
 function getEnd(idUser, callback) {
+
   setTimeout(() => {
     return callback(null, {
       street: 'Av. Central',
       number: 0
     })
-  }, 2000)
+  }, 1000)
+
 }
 
-function userResolve(error, user) {
-  console.log('usuario', user)
-}
+const userPromise = getUser() 
 
-getUser(function userResolve(error, user) {
-  // null || "" || 0 === false
-  if(error) {
-    console.error('Problema com o usuario', error)
-    return;
-  }
-
-  getTel(user.id, function telResolve(error1, tel) {
-    if(error1) {
-      console.error('Problema com o telefone', error)
-      return;
-    }
-    getEnd(user.id, function endResolve(error2, end) {
-      if(error2) {
-        console.error('Problema com o endereço', error)
-        return;
+userPromise
+  .then(function (user) {
+    return getTel(user.id)
+      .then(function resolveTel(result) {
+        return {
+          usuario: {
+            name: user.name,
+            id: user.id
+          },
+          tel: result
+        }
+      })
+  })  
+  .then(function (resultado) {
+    const end = getEndAsync(resultado.id)
+    return end.then(function resolveEnd(result) {
+      return {
+        usuario: resultado.usuario,
+        telefone: resultado.tel,
+        endereco: result
       }
-      console.log(`
-        name: ${user.name},
-        end: ${end.street}, ${end.number}
-        tel: (${tel.ddd}) ${tel.tel}
-      `)
     })
   })
-}) 
+  .then(function (resultado) {
+    console.log(`
+      Nome: ${resultado.usuario.name}
+      Endereco: ${resultado.endereco.street}, ${resultado.endereco.number}
+      Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.tel}
+    `)
+  })
+  .catch(function (error) {
+    console.error('Error', error)
+  })
 
-//const tel = getTel(usuario.id)
-
-//console.log('telefone', tel)
--- addtwo.js
+++
@@ -1,9 +0,0 @@
-var base = 2;
-
-exports.addtwo = function(input) {
-  return parseInt(input) + base;
-} 
-
-var addtwo = require('./addtwo').addtwo;
-var base = 10;
-console.log(addtwo(base));
