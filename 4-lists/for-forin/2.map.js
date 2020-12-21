24778002231e16e6eabe7b511cf94b867dd8142f
Daniel Alves <danielalves.ccomp@gmail.com>
2020-12-21 19:00:17 -0300

    Creating array map

---
+++ 4-lists/for-forin/2.map.js
@@ -0,0 +1,37 @@
+const service = require('./service')
+
+Array.prototype.myMap = function (callback) {
+  const newArrayMap = []
+  for(let i = 0; i <= this.length - 1; i++) {
+    const result = callback(this[i], i)
+    newArrayMap.push(result)
+  }
+
+  return newArrayMap;
+
+}
+
+async function main() {
+  try {
+    const results = await service.getPeople(`a`)
+    //const names = []
+    // results.results.forEach((item) => {
+    //   names.push(item.name)
+    // })
+
+    // const names = results.results.map((pessoa) => {
+    //   return pessoa.name
+    // })
+
+    const names = results.results.myMap((pessoa, i) => {
+      return `[${i}]${pessoa.name}`
+    })
+
+    console.log('names', names)
+  } catch (error) {
+    console.error(error)
+  }
+
+}
+
+main()
