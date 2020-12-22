a2f861a6419b0b1ba4de7f10337eaad557e4baec
Daniel Alves <danielalves.ccomp@gmail.com>
2020-12-22 18:44:38 -0300

    Create reduce

--- 4-lists/for-forin/4.reduce.js
+++ 4-lists/for-forin/4.reduce.js
@@ -1,5 +1,13 @@
 const { getPeople } = require('./service')
 
+Array.prototype.myReduce = function(callback, initialValue) {
+  let finalValue = typeof initialValue !== undefined ? initialValue : this[0]
+  for(let i = 0; i <= this.length - 1; i++) {
+    finalValue = callback(finalValue, this[i], this)
+  }
+  return finalValue
+}
+
 async function main() {
   try {
     const { results } = await getPeople(`a`)
@@ -7,9 +15,18 @@ async function main() {
     
     console.log('Alturas:', height)
 
-    const total = height.reduce((ant, prox) => {
-      return ant + prox
-    })
+    // const total = height.reduce((ant, prox) => {
+    //   return ant + prox
+    // })
+
+    const myList = [
+      ['Daniel', 'Andrew'],
+      ['NodeBR', 'ReactJS']
+    ]
+    const total = myList.myReduce((ant, prox) => {
+      return ant.concat(prox)
+    }, [])
+    .join(', ')
 
     console.log('Total:', total)
   } catch (error) {
