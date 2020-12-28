75bcd2ac8345737b9ebffdee88843c153cd03de0
Daniel Alves <danielalves.ccomp@gmail.com>
2020-12-28 01:28:56 -0300

    Manilulating files - Create

--- 6-cli/test.js
+++ 6-cli/test.js
@@ -8,15 +8,21 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe('Suite de manipulação de Herois', () => {
+  before(async () => {
+    await database.register(DEFAULT_ITEM_CADASTRAR)
+  })
   it('deve pesquisar um heroi usando arquivos', async () =>{
     const expected = DEFAULT_ITEM_CADASTRAR
     const [result]= await database.list(expected.id)
 
     deepEqual(result, expected)
   })
-  // it('deve cadastrar um heroi, usando arquivos', async () => {
-  //   const expect = DEFAULT_ITEM_CADASTRAR
 
-  //   ok(null, expect)
-  // })
+  it('deve cadastrar um heroi, usando arquivos', async () => {
+    const expect = DEFAULT_ITEM_CADASTRAR
+    const result = await database.register(DEFAULT_ITEM_CADASTRAR)
+    const [current] = await database.list(DEFAULT_ITEM_CADASTRAR.id)
+
+    deepEqual(current, expect)
+  })
 }) 
